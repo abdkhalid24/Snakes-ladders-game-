6032eb8d894026bb33c98a53c7ae85dd3aeee7c4
abdkhalid24 <busaud973@gmail.com>
2025-07-12 23:09:42 +0300

    I have added comments to the java script file for most of the lines.

--- num.js
+++ num.js
@@ -3,6 +3,7 @@ let massageNum = document.querySelector("#numMassage");
 let attemptsNum = document.querySelector(".atemptsDisplay");
 let secretNum = Math.round(Math.random() * 50);
 let restartButton = document.querySelector(".restart");
+// Number of attempts is 5 for each round
 let attempts = 0;
 let maxAttempts = 5;
 
@@ -16,29 +17,28 @@ const numberAttempts = () => {
 const secretStatement = () => {
   let guessNum = document.querySelector("#guessBar").value;
 
-  if (guessNum == secretNum) {
-    massageNum.innerHTML = "You're correct! good Job :)";
-  } else if (guessNum > secretNum && guessNum <= 50) {
-    massageNum.innerHTML = "Too high";
+  if (guessNum == secretNum) { 
+    massageNum.innerHTML = "You're correct! good Job :)"; // if guessNum is equal to secret
+  } else if (guessNum > secretNum && guessNum <= 50) { 
+    massageNum.innerHTML = "Too high"; //if guessNum between the main number and less than 50
   } else if (guessNum < secretNum && guessNum <= 50 && guessNum >= 1) {
-    massageNum.innerHTML = "Too low";
+    massageNum.innerHTML = "Too low"; //if guessNum less than secret and in the range of 1 to 50
   } else if (guessNum > secretNum && guessNum > 50) {
-    massageNum.innerHTML = "You've gone too far, enter a number between 1-50";
+    massageNum.innerHTML = "You've gone too far, enter a number between 1-50"; // if guessNum more than 50
   } else if (guessNum < secretNum && guessNum < 1) {
-    massageNum.innerHTML = "You've landed far, enter a number between 1-50";
+    massageNum.innerHTML = "You've landed far, enter a number between 1-50"; // if guessNum less than 50
   }
 // ...calling the attempts after feedback...
   numberAttempts(); 
-  let submitBar = document.querySelector(".submitBar");
-  let video = document.querySelector(".video");
+  let submitBar = document.querySelector(".submitBar"); // the bar of submission
 
   if (attempts >= maxAttempts) {
     massageNum.textContent = "You've reached the maximum number of attempts!";
     submitBar.disabled = true;
-    video.play();
-    return;
-  }
+    // when player reached the maximum num of attempts
+    
 };
+}
 
 // ...Restart statement...
 document.addEventListener("DOMContentLoaded", function () {
@@ -50,4 +50,4 @@ document.addEventListener("DOMContentLoaded", function () {
     location.reload();
 
   });
-});
+})
